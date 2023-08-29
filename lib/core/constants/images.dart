0a50a51b53b5c58f61fab1463f17b7671acfd5b1
AjiGobimble2023 <ajimassaid.gobimbel@gmail.com>
2023-08-29 09:12:39 +0700

    remove icon and image are not use

--- lib/core/constants/images.dart
+++ lib/core/constants/images.dart
@@ -2,46 +2,5 @@ part of 'package:campus_app/core.dart';
 
 class AppImages {
   static const String logo = 'assets/images/logo.png';
-  static const String voucher = 'assets/images/voucher.jpg';
-
-  static const String cardInfo1 = 'assets/images/cardInfo1.png';
-  static const String cardInfo2 = 'assets/images/cardInfo2.png';
-  static const String cardInfo3 = 'assets/images/cardInfo3.png';
-  static const String cardInfo4 = 'assets/images/cardInfo4.png';
-  static const String cardInfo5 = 'assets/images/cardInfo5.png';
-  static const String cardInfo6 = 'assets/images/cardInfo6.png';
-  static const String cardInfo7 = 'assets/images/cardInfo7.png';
-
-  static const String productShop1 = 'assets/images/productShop1.png';
-  static const String productShop2 = 'assets/images/productShop2.png';
-  static const String productShop3 = 'assets/images/productShop3.png';
-  static const String productShop4 = 'assets/images/productShop4.png';
-  static const String productShop5 = 'assets/images/productShop5.png';
-  static const String productShop6 = 'assets/images/productShop6.png';
-  static const String productShop7 = 'assets/images/productShop7.png';
-
-  static const String cardShop1 = 'assets/images/cardShop1.png';
-  static const String cardShop2 = 'assets/images/cardShop2.png';
-  static const String cardShop3 = 'assets/images/cardShop3.png';
-
-  static const String bannerShop1 = 'assets/images/bannershop1.png';
-  static const String bannerShop2 = 'assets/images/bannershop2.png';
-  static const String bannerShop3 = 'assets/images/bannershop3.jpg';
-
-  static const String emptyState = 'assets/images/emptyState.png';
   static const String emptyKeranjang = 'assets/images/emptyKeranjang.png';
-
-  static const String homeInfo = 'assets/images/homeInfo.png';
-  static const String homeShop = 'assets/images/homeShop.png';
-
-  static const String loadingPembayaran = 'assets/images/loadingPembayaran.png';
-
-  static const String bca = "assets/icons/bca.png";
-  static const String bri = "assets/icons/bri.png";
-  static const String bni = "assets/icons/bni.png";
-  static const String mandiri = "assets/icons/mandiri.png";
-  static const String ovo = "assets/icons/ovo.png";
-  static const String gopay = "assets/icons/gopay.png";
-  static const String dana = "assets/icons/dana.png";
-  static const String shopeePay = "assets/icons/shopeePay.png";
 }
